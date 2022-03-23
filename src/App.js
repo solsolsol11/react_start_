45d3d08a82f170e5bf711b4d29b8e96cf99bc7db
solsolsol11 <ushio941@gmail.com>
2022-03-23 14:14:21 +0900

    현재의 자산으로 얼마만큼의 코인을 살 수 있는지 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers") // 정보 가져올 url
+      .then((response) => response.json()) // url에서 가져온 정보를 json으로 변환
+      .then((json) => {
+        setCoins(json); // 변환된 json을 coins(빈 array)에 넣기
+        setLoading(false); // loading 글자 지우기
+      });
+  }, []);
   return (
+    //함수 사이에 string 삽입 시: `~`, string 사이에 인자나 함수 삽입 시: ${~}
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Wrote your to do ...."
-        />
-
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!{loading ? "" : `${coins.length}`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
+        <label htmlFor="USD">$USD :</label>
+        <input value={setMoney} id="USD" type="number" />
+      </div>
+      <div>
+        HOW :<input id="" type="text" disabled />
+      </div>
     </div>
   );
 }
